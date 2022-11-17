18228a6eda393fa8ca47fd6d47b7dec58b2fcbc2
Mehmood Ghaffar <in1767@kit.edu>
2022-11-17 15:34:44 +0100

    updated cypress.config with default user.props

--- cypress.config.js
+++ cypress.config.js
@@ -7,7 +7,14 @@ module.exports = defineConfig({
   e2e: {
     env: {
       login_url: 'http://localhost:' + port + '/users/sign_in',
-      TARGET : 'http://localhost:' + port
+      TARGET : 'http://localhost:' + port,
+			reservedList: ["CRR","CRS","CRD"],
+      lengthGroup: [2, 5],
+      lengthDevice: [2, 6],
+      lengthDefault: [2, 8],
+      formatAbbr: "!ruby/regexp '/\A[a-zA-Z][a-zA-Z0-9\-_]*[a-zA-Z0-9]\Z/'",
+      formatAbbrErrMsg: "can be alphanumeric, middle '_' and '-' are allowed, but leading digit, or trailing '-' and '_' are not."
+
     },
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
